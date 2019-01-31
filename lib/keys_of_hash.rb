2abfafc2c595eee4e9d059cdc913dccd0c501e1e
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-01-31 08:23:03 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,8 +1,10 @@
 class Hash
   def keys_of(*arguments)
-    
+    a = 0 
     n = 0 
     array = []
+    
+    p arguments.length
     
     while n < values.length 
       if arguments[0] == values[n]
