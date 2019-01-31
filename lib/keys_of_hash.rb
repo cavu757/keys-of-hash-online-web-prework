eb60df8188ac08500dd3e57b2ffb63084bcf665f
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-01-31 08:24:31 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,14 +4,15 @@ class Hash
     n = 0 
     array = []
     
-    p arguments.length
-    
-    while n < values.length 
-      if arguments[0] == values[n]
-        p array << keys[n]
+    while a < arguments.length
+      while n < values.length 
+        if arguments[0] == values[n]
+          p array << keys[n]
+        end
+        n += 1 
       end
-      n += 1 
-    end
+      a += 1 
+    end 
     array 
   end
 end
