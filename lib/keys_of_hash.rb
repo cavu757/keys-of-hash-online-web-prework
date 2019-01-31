029f0d6555197a7425cea3729360097c7eb056e2
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-01-31 08:31:34 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    a = 0 
+    array = []
+    while a < arguments.length
+      n = 0 
+      while n < values.length 
+        if arguments[a] == values[n]
+          array << keys[n]
+        end
+        n += 1 
+      end
+      a += 1 
+    end 
+    array 
   end
 end
